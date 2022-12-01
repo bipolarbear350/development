af03577c66e6126b82f588cb713320f18d384260
ansoson <115022050+bipolarbear350@users.noreply.github.com>
2022-12-01 17:38:11 -0500

    in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import bakeryData from "./data/bakery-data.json";
+import {BakeryItem} from "./components/BakeryItem";
+import {Grid} from "@mui/material";
+import {Container} from "@mui/material";
+import {blue, red} from "@mui/material/colors";
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
+bakeryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
 
 function App() {
+
+  const [finalPrice, setFinalprice] = useState(0);
+  function handlePriceClick(input){
+    setFinalprice(finalPrice + input)
+  }
+
+  const [cartList, setCartList] = useState([]);
+  function addToCart(input1, input2) {
+    setCartList(cartList.concat(input1))
+    handlePriceClick(input2)
+  }
+
+  const [glutenChecked, setGlutenChecked] = useState(false);
+  function checkGluten() {
+      if (glutenChecked){setGlutenChecked(false)}
+      else{setGlutenChecked(true)}
+  }
+
+    const [vegetarianChecked, setVegetarianChecked] = useState(false);
+    function checkVegetarian() {
+        if (vegetarianChecked){setVegetarianChecked(false)}
+        else{setVegetarianChecked(true)}
+    }
+
+    const [veganChecked, setVeganChecked] = useState(false);
+    function checkVegan() {
+        if (veganChecked){setVeganChecked(false)}
+        else{setVeganChecked(true)}
+    }
+
+    const [timeChecked, setTimeChecked] = useState(false);
+    function checkTime(input) {
+        if (timeChecked){setTimeChecked(false)}
+        else{setTimeChecked(true)}
+    }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className={"HeaderTitle"}>My Recipe Catalog</header>
+        <FormGroup className={'mainOptions'}>
+            <FormControlLabel control={<Checkbox onChange={checkGluten}/>} label="Gluten-free" />
+            <FormControlLabel control={<Checkbox onChange={checkVegetarian} />} label="Vegetarian" />
+            <FormControlLabel control={<Checkbox onChange={checkVegan} />} label="Vegan" />
+        </FormGroup>
+      <Grid container justifyContent={"center"}>
+      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          <BakeryItem item={item} itemToCart={addToCart} glutenTag={glutenChecked}
+          vegTag={vegetarianChecked} veganTag={veganChecked}/>
+      ))}
+      </Grid>
+
+      <div>
+        <h2>Your Current plan</h2>
+        {/* TODO: render a list of items in the cart */}
+          <p>Appetizers</p>
+          <Grid container></Grid>
+          <p>Main Course</p>
+        <p>Items: {cartList}</p>
+        <p>Total price: {finalPrice}</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/BakeryItem.js
@@ -0,0 +1,147 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Button from '@mui/material/Button';
+
+let description;
+let image;
+let title;
+let altText;
+let vegStatus = false;
+let veganStatus = false;
+let gluStatus = false;
+let extLink;
+let prepTime;
+let time;
+
+export function BakeryItem({item, itemToCart, glutenTag, vegTag, veganTag}) {
+    description = item.description;
+    image = item.image;
+    title = item.name;
+    altText = item.alt;
+    gluStatus = item.gluten;
+    vegStatus = item.vegetarian;
+    veganStatus = item.vegan;
+    extLink = item.link;
+    prepTime = item.prepTime;
+    time = item.time;
+
+
+    let status = [false, false, false];
+
+    if (glutenTag && vegTag && veganTag) {
+        if (gluStatus && vegStatus && veganStatus) {
+            return(createCard());
+        }
+    } else if (glutenTag && veganTag) {
+        if (gluStatus && veganStatus) {
+            return(createCard());
+        }
+    } else if (glutenTag && vegTag) {
+        if (gluStatus && vegStatus) {
+            return(createCard());
+        }
+    } else if (vegTag && veganTag) {
+        if (vegStatus && veganStatus) {
+            return(createCard());
+        }
+    } else if (glutenTag) {
+        if (gluStatus) {
+            return(createCard());
+        }
+    } else if (vegTag) {
+        if (vegStatus) {
+            return(createCard());
+        }
+    } else if (veganTag) {
+        if (veganStatus) {
+            return(createCard());
+        }
+    } else if (!glutenTag && !vegTag && !veganTag) {
+        return(createCard());
+    }
+
+
+
+    // switch (status)
+    // {
+    //     case (true, true, true):
+    //         if (gluStatus && vegStatus && veganStatus)
+    //         return(createCard());
+    //         break;
+    //     case (true, true, false):
+    //         if (gluStatus && vegStatus)
+    //             return(createCard());
+    //         break;
+    //     case (true, false, true):
+    //         if (gluStatus && veganStatus)
+    //             return(createCard());
+    //         break;
+    //     case (false, true, true):
+    //         if (vegStatus && veganStatus)
+    //             return(createCard());
+    //         break;
+    //     case (true, false, false):
+    //         if (gluStatus)
+    //             return(createCard());
+    //         break;
+    //     case (false, true, false):
+    //         if (vegStatus)
+    //             return(createCard());
+    //         break;
+    //     case (false, false, true):
+    //         if (veganStatus)
+    //             return(createCard());
+    //         break;
+    //     case (status):
+    //             return(createCard());
+    // }
+
+    function testAppend() {
+        let labelList = "-"
+        if (gluStatus) {labelList = labelList.concat("- Gluten-free -")}
+        if (vegStatus) {labelList = labelList.concat("- Vegetarian -")}
+        if (veganStatus) {labelList = labelList.concat("- Vegan -")}
+        labelList = labelList.concat("-")
+        return labelList;
+    }
+
+    function createCard() {
+        return(
+            <Card sx={{ width: 345 }}>
+                <CardActionArea href={extLink}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image= {image}
+                        alt= {altText}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {description}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {prepTime}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {">o<"}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {testAppend()}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardContent>
+                    <Button variant="outlined">Add/Remove From Plan</Button>
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
